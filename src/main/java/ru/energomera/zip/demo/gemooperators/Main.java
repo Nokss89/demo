da2db527504a86ae3ae2bae59d02fb261fece845
Nokss89 <noks89@gmail.com>
2021-02-19 20:37:25 +0300

    change hello message

--- src/main/java/ru/energomera/zip/demo/gemooperators/Main.java
+++ src/main/java/ru/energomera/zip/demo/gemooperators/Main.java
@@ -3,6 +3,6 @@ package ru.energomera.zip.demo.gemooperators;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, WORlD!!!");
+        System.out.println("Hello, energomera!!!");
     }
 }
